e0f0080ff71d75fedd953ad547b7350fd3a3f9a9
mk9140 <31506088+mk9140@users.noreply.github.com>
2022-03-30 22:09:11 +0900

    button click -> select section

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const sections = document.querySelectorAll('.section'); // 'section' クラス
 const sectBtns = document.querySelectorAll('.controls'); // 'controls' クラス
 const sectBtn = document.querySelectorAll('.control'); // 'control' クラス(各ボタン)
-const allSections = document.querySelectorAll('.main-content'); // 'main-content' クラス(body)
+const allSections = document.querySelector('.main-content'); // 'main-content' クラス(body)
 
 function PageTransitions() {
-	// ボタンクリック→アクティブ
+	// ボタンクリック→'active-btn'クラス追加してボタンク活性化
 	for (let i = 0; i < sectBtn.length; i++) {
 		sectBtn[i].addEventListener('click', function() {
 			let currentBtn = document.querySelectorAll('.active-btn');
@@ -12,6 +12,27 @@ function PageTransitions() {
 			this.className += 'active-btn';
 		})
 	}
+
+	// ボタンクリック→sectionアクティブ
+	allSections.addEventListener('click',(event) => {
+		const id = event.target.dataset.id;
+		if (id) {
+			// 他のボタンクからsectionアクティブを落とす
+			sectBtns.forEach((btn)=>{
+				btn.classList.remove('active-btn')
+			})
+			event.target.classList.add('active-btn')
+
+			//他のsectionを隠す
+			sections.forEach((section) => {
+				section.classList.remove('active')
+			})
+
+			//選択したsectionをアクティブ
+			const element = document.getElementById(id);
+			element.classList.add('active')
+		}
+	})
 }
 
 PageTransitions();
